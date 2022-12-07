39d8c92859826081d8c2533500559c8372edc3c9
dacorm <stolen.youth@yandex.ru>
2022-12-07 20:54:01 +0700

    feat: first layout

---
+++ src/components/NavTab/NavTab.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavTab.css';
+
+export const NavTab = () => {
+    return (
+        <div className='nav'>
+            <ul className='nav__items'>
+                <li className='nav__item'></li>
+                <li className='nav__item'></li>
+                <li className='nav__item'></li>
+            </ul>
+        </div>
+    );
+};
+
---
+++ src/components/Promo/Promo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Promo.css';
+
+export const Promo = () => {
+    return (
+        <div className='promo'>
+            <h1 className='promo__title'>Учебный проект студента факультет веб разработки.</h1>
+        </div>
+    );
+};
