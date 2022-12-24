81891c217eec668d40ce1866f7a664581576ba7b
dacorm <stolen.youth@yandex.ru>
2022-12-25 01:16:29 +0700

    feat: lend adaptive

--- src/components/Promo/Promo.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Promo.css';
-
-export const Promo = () => (
-    <div className="promo">
-        <h1 className="promo__title">Учебный проект студента факультет веб разработки.</h1>
-    </div>
-);
